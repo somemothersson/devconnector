d276d870732134e5902a3b99d3f058c47ddb1ab4
Stephen Malito <stevemalito@gmail.com>
2019-08-13 21:54:32 -0500

    education and experiences populating to the dashboard

--- client/src/components/profile-form/AddEducation.js
+++ client/src/components/profile-form/AddEducation.js
@@ -122,4 +122,4 @@ AddEducation.propTypes = {
 export default connect(
   null,
   { addEducation}
-)(AddEducation);
+)(withRouter(AddEducation));
--- client/src/components/profile-form/AddExperience.js
+++ client/src/components/profile-form/AddExperience.js
@@ -123,4 +123,4 @@ AddExperience.propTypes = {
 export default connect(
   null,
   { addExperience }
-)(AddExperience);
+)(withRouter(AddExperience));
