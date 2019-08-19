2eed4800ac20e7c822466a35f7ac57d6436850bb
Stephen Malito <stevemalito@gmail.com>
2019-08-18 20:31:42 -0500

    added redux prep for profile listings

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -6,7 +6,9 @@ import {
   PROFILE_ERROR,
   UPDATE_PROFILE,
   CLEAR_PROFILE,
-  ACCOUNT_DELETED
+  ACCOUNT_DELETED,
+  GET_PROFILES,
+  GET_REPOS
 } from "./types";
 
 export const getCurrentProfile = () => async dispatch => {
@@ -14,6 +16,57 @@ export const getCurrentProfile = () => async dispatch => {
     const res = await axios.get("/api/profile/me");
     dispatch({
       type: GET_PROFILE,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Get all Profiles
+export const getProfiles = () => async dispatch => {
+  dispatch({ type: CLEAR_PROFILE })
+  try {
+    const res = await axios.get("/api/profile");
+    dispatch({
+      type: GET_PROFILES,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Get Profile by ID
+export const getProfileById = userId => async dispatch => {
+ 
+  try {
+    const res = await axios.get(`/api/profile/user/${userId}`);
+    dispatch({
+      type: GET_PROFILES,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Get githubrepos
+export const getGithubRepos = username => async dispatch => {
+ 
+  try {
+    const res = await axios.get(`/api/profile/github/${username}`);
+    dispatch({
+      type: GET_REPOS,
       payload: res.data
     });
   } catch (err) {
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,11 @@
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE } from "../actions/types";
+import {
+  GET_PROFILE,
+  PROFILE_ERROR,
+  CLEAR_PROFILE,
+  UPDATE_PROFILE,
+  GET_PROFILES,
+  GET_REPOS
+} from "../actions/types";
 
 const initialState = {
   profile: null,
@@ -19,17 +26,29 @@ export default function(state = initialState, action) {
         profile: payload,
         loading: false
       };
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: payload,
+        loading: false
+      };
     case PROFILE_ERROR:
       return {
         ...state,
         error: payload,
         loading: false
       };
-      case CLEAR_PROFILE:
+    case CLEAR_PROFILE:
+      return {
+        ...state,
+        profile: null,
+        repos: [],
+        loading: false
+      };
+      case GET_REPOS:
         return {
           ...state,
-          profile: null,
-          repos: [],
+          repos: payload,
           loading: false
         }
     default:
