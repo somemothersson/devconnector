7268675bff6b2d7b49c00b96ec372983438df7b0
Stephen Malito <stevemalito@gmail.com>
2019-08-18 23:58:07 -0500

    added github repos

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -5,8 +5,9 @@ import { connect } from "react-redux";
 import Spinner from "../layouts/Spinner";
 import ProfileTop from "./ProfileTop";
 import ProfileAbout from "./ProfileAbout";
-import ProfileExperience from "./ProfileExperience"
-import ProfileEducation from "./ProfileEducation"
+import ProfileExperience from "./ProfileExperience";
+import ProfileEducation from "./ProfileEducation";
+import ProfileGithub from "./ProfileGithub";
 import { getProfileById } from "../../actions/profile";
 
 const Profile = ({
@@ -53,21 +54,25 @@ const Profile = ({
                 <h4>No Experience Credentials</h4>
               )}
             </div>
-            <div className="profile-edu bg-white p-2">
-                <h2 className="text-primary">Education</h2>
-                {profile.education.length > 0 ? (
-                    <Fragment>
-                        {profile.education.map(education => (
-                        <ProfileEducation 
-                        key={education._id}
-                        education={education}
-                        />
-                        ))}
-                </Fragment> ) : (
-                    <h4>No Education Experience</h4>
-                
-                )}
+            <div className='profile-edu bg-white p-2'>
+              <h2 className='text-primary'>Education</h2>
+              {profile.education.length > 0 ? (
+                <Fragment>
+                  {profile.education.map(education => (
+                    <ProfileEducation
+                      key={education._id}
+                      education={education}
+                    />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No Education Experience</h4>
+              )}
             </div>
+
+            {profile.githubusername && (
+                <ProfileGithub username={profile.githubusername}/>
+            )}
           </div>
         </Fragment>
       )}
--- client/src/components/profile/ProfileEducation.js
+++ client/src/components/profile/ProfileEducation.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import Moment from "react-moment";
 
 const ProfileEducation = ({
-    education: { school, degree, location, current, to, from, description }
+    education: { school, degree, fieldofstudy, current, to, from, description }
   }) => (
     <div>
       <h3 className='text-dark'>{school}</h3>
@@ -12,7 +12,10 @@ const ProfileEducation = ({
         {!to ? "Now" : <Moment format=' YYYY/MM/DD'>{to}</Moment>}
       </p>
       <p>
-          <strong>Position: </strong> {degree}
+          <strong>Degree: </strong> {degree}
+      </p>
+      <p>
+          <strong>Field of Study: </strong> {fieldofstudy}
       </p>
       <strong>Description:</strong>{description}
     </div>
