8425eba4e689387494bf1d5a69ea0363f705dee2
Stephen Malito <stevemalito@gmail.com>
2019-10-31 18:17:36 -0500

    changed request-promise to request in profile api route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
 const { check, validationResult } = require("express-validator");
-const request = require("request-promise");
+const request = require("request");
 const config = require("config");
 
 const Profile = require("../../models/profile");
