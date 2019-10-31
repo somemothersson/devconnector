8b07ecf329a14eaee8815fe21fcc8bc817c7526e
Stephen Malito <stevemalito@gmail.com>
2019-10-31 17:42:17 -0500

    Create CommentItem, comments loading under posts

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -6,6 +6,7 @@ import PostItem from "../posts/PostItem";
 import { getPost } from "../../actions/post";
 import { Link } from "react-router-dom";
 import CommentForm from './CommentForm'
+import CommentItem from './CommentItem'
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
   useEffect(() => {
@@ -18,6 +19,11 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
         <Link to="/posts" className='btn'>Back to Posts</Link>
       <PostItem post={post} showActions={false} />
       <CommentForm postId={post._id} />
+      <div className="comments">
+          {post.comments.map(comment => (
+              <CommentItem key={comment.id} comment={comment} postId={post._id} />
+          ))}
+      </div>
     </Fragment>
   );
 };
